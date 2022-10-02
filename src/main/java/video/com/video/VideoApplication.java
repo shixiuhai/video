5c864876f6ab012060d56653406ae4dcc28eee4b
shixiuhai <15256728901@163.com>
2022-10-02 12:05:54 +0800

    改正了VideoApplication

--- src/main/java/video/com/video/VideoApplication.java
+++ src/main/java/video/com/video/VideoApplication.java
@@ -2,10 +2,6 @@ package video.com.video;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
-=======
-// 添加了不必要的注释用来测试
->>>>>>> 16a6a76911a90be6e30904af07b4288d22c0527e
 @SpringBootApplication
 public class VideoApplication {
 
